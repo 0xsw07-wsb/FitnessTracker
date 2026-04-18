11bdb86b34e7175fd72c9e77292f0a44b3511e7b
0xsw07 <woj.seba02@gmail.com>
2026-04-18 10:23:53 +0200

    Lab3 done

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,5 +1,47 @@
 package pl.wsb.fitnesstracker.event;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "event")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "startTime", nullable = false)
+    private LocalDate startTime;
+
+    @Column(name = "endTime")
+    private LocalDate endTime;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "city")
+    private String city;
+
+    public Event(final String name, final LocalDate startTime, final String city) {
+        this.name = name;
+        this.startTime = startTime;
+        this.city = city;
+    }
 }
